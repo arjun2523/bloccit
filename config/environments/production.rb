e7e8e882a8e0ca2a0fe0971c9a6e8d73cf457f83
Arjun Srivastava <arjun.srivastava2592@gmail.com>
2014-07-31 20:59:58 +0000

    Configured app for Production emails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,3 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+# http://arjun-bloccit.herokuapp.com
+   config.action_mailer.default_url_options = { host: 'arjun-bloccit.herokuapp.com' }
+end
